54429600a622da0c6a791ba68a6fbb90618fb88b
wmdvanzyl <willem@glyph.capetown>
2022-02-14 21:48:05 +0200

    Update Home.js

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -3,20 +3,20 @@ import React, {useEffect} from 'react';
 import './Home.scss';
 import agent from '../../agent/agent';
 import MainForm from '../MainForm/MainForm';
-//import MainForm from '../MainForm/MainForm';
 
 const Home = () => {
 
   //React.useEffect(() => {
   useEffect(() => {
-    agent.Crawler.getHistory();//TODO: remove semicolon when uncommenting code below
-      /*.then(res => {
+    agent.Crawler.getHistory()//TODO: remove semicolon when uncommenting code below
+      .then(res => {
         // Do something on successful getHisory() call
+        console.log("Success!" + res);
 
       }).catch(e => {
         // Handle the error if unsuccessful getHisory() call
-
-      });*/
+        console.log("Failure" + e);
+      });
   });
 
   return (
